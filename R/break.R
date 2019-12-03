5de5ae1a18321e28c5bfe85ae076a49fcb38f317
Raymond Wong <raymondkww.dev@gmail.com>
2019-12-03 10:14:15 -0600

    update

--- R/break.R
+++ R/break.R
@@ -2,8 +2,13 @@
 #### computation of MDL ####
 ############################
 
+# chrom0stat is a list:
+# chrom0stat$chrom
+# chrom0stat$loglike
+# chrom0stat$mdl.pen
+
 MDL <- function(chrom, x1, x2, y, A, delta.t, delta.w, opt=get.opt.spec(),
-                min.span=5, assign.emiss=NA, emdl=TRUE, v1=1, v2=1){
+                min.span=5, assign.emiss=NA, emdl=TRUE, v1=1, v2=1, chrom0stat=NULL){
   minx1 <- min(x1)-1
   maxx1 <- max(x1)+1
 
@@ -13,17 +18,52 @@ MDL <- function(chrom, x1, x2, y, A, delta.t, delta.w, opt=get.opt.spec(),
   if (min(x1.len)>=min.span){
     mdl.pen <- array(dim=B)
     loglike <- array(dim=B)
-    for (k in (1:B)){
+
+    ## use chrom0stat or not
+    if (is.null(chrom0stat)){
+      ks <- 1:B
+    } else {
+      cres <- compare.chroms(chrom, chrom0stat$chrom)
+      ks <- which(is.na(cres))
+      for (j in which(!is.na(cres))){
+        mdl.pen[j] <- chrom0stat$mdl.pen[cres[j]]
+        loglike[j] <- chrom0stat$loglike[cres[j]]
+      }
+    }
+
+    for (k in ks){
       tempy <- apply(y[,x1.index==k],1,sum)
       tempfit <- spec(x=x2, y=tempy, A=A, delta.t=delta.t, delta.w=delta.w,reps=x1.len[k],display=F,opt=opt,assign.emiss=assign.emiss,emdl=emdl,v=v1,simple=T)
-      loglike[k] <- sum(dpois(as.vector(y[,x1.index==k]), lambda=rep(exp(tempfit$off.const+tempfit$fitted.bright-log(x1.len[k])),x1.len[k]),log=T))
+      if (is.infinite(tempfit$mdl.pen[tempfit$mdl.ind[1],tempfit$mdl.ind[2]])){
+        return(list(mdl=Inf))
+      }
+      loglike[k] <- sum(dpois(as.vector(y[,x1.index==k]), lambda=rep(exp(tempfit$off.const+tempfit$fitted.logbright-log(x1.len[k])),x1.len[k]),log=T))
       mdl.pen[k] <- tempfit$mdl.pen[tempfit$mdl.ind[1],tempfit$mdl.ind[2]]
     }
     #-sum(loglike)+sum(mdl.pen)++B*log(n1)+sum(log(x1.len))
-    -sum(loglike)+sum(mdl.pen)*v1+(log(B)+sum(log(x1.len)))*v2
+    list(mdl=-sum(loglike)+sum(mdl.pen)*v1+(log(B)+sum(log(x1.len)))*v2,
+         loglike=loglike, mdl.pen=mdl.pen)
   } else {
-    Inf
+    list(mdl=Inf)
   }
+}
+
+
+# detect the same pieces in chrom0
+compare.chroms <- function(chrom, chrom0){
+  bs <- c(1, which(chrom), length(chrom)+1)
+  bs0 <- c(1, which(chrom0), length(chrom0)+1)
+  B <- sum(chrom)+1
+  matchid <- array(dim=B)
+  for (i in (1:B)){
+    j <- which(bs0==bs[i])
+    if (length(j)>0){
+      if (bs[i+1] == bs0[j+1]){
+        matchid[i] <- j
+      }
+    }
+  }
+  return(matchid)
 }
 
 ###################################################################
@@ -48,7 +88,10 @@ spec.tbreak <- function(x1, x2, y, A, delta.t, delta.w,
   breaks.ind <- ((min.span+1):(n1-min.span+1))-1
   cbreaks <- c()
   #old.min.criteria <- Inf
-  old.min.criteria <- MDL(chrom=rep(F,(n1-1)),x1=x1,x2=x2,y=y,A=A,delta.t=delta.t,delta.w=delta.w,opt=opt,min.span=min.span,assign.emiss=assign.emiss,emdl=emdl, v1=v1, v2=v2)
+  res <- MDL(chrom=rep(F,(n1-1)),x1=x1,x2=x2,y=y,A=A,delta.t=delta.t,delta.w=delta.w,opt=opt,min.span=min.span,assign.emiss=assign.emiss,emdl=emdl, v1=v1, v2=v2, chrom0stat=NULL)
+  old.min.criteria <- res$mdl
+  best.criteria.seq <- old.min.criteria
+  chrom0stat <- list(chrom=rep(F,(n1-1)), loglike=res$loglike, mdl.pen=res$mdl.pen)
   while ((nbreaks<=(max.B-1))&&cont){
 
     S <- length(breaks.ind)
@@ -58,23 +101,29 @@ spec.tbreak <- function(x1, x2, y, A, delta.t, delta.w,
       pop[i,breaks.ind[i]] <- T
     }
 
+    cat(nbreaks,"\n")
     # compute mdl
     if (cpus>1){
-      criteria <- foreach::foreach(mci = 1:S,.combine=c,.verbose=display) %dopar% {
-        MDL(chrom=pop[mci,],x1=x1,x2=x2,y=y,A=A,delta.t=delta.t,delta.w=delta.w,opt=opt,min.span=min.span,assign.emiss=assign.emiss,emdl=emdl, v1=v1, v2=v2)
+      #criteria
+      res <- foreach::foreach(mci = 1:S,.verbose=display) %dopar% {
+        MDL(chrom=pop[mci,],x1=x1,x2=x2,y=y,A=A,delta.t=delta.t,delta.w=delta.w,opt=opt,min.span=min.span,assign.emiss=assign.emiss,emdl=emdl, v1=v1, v2=v2, chrom0stat=chrom0stat)
       }
     } else {
-      criteria <- array(dim=S)
+      #criteria <- array(dim=S)
+      res <- list()
       for (mci in (1:S)){
-        criteria[mci] <- MDL(chrom=pop[mci,],x1=x1,x2=x2,y=y,A=A,delta.t=delta.t,delta.w=delta.w,opt=opt,min.span=min.span,assign.emiss=assign.emiss,emdl=emdl, v1=v1, v2=v2)
+        #criteria[mci]
+        res[[mci]] <- MDL(chrom=pop[mci,],x1=x1,x2=x2,y=y,A=A,delta.t=delta.t,delta.w=delta.w,opt=opt,min.span=min.span,assign.emiss=assign.emiss,emdl=emdl, v1=v1, v2=v2, chrom0stat=chrom0stat)
       }
     }
 
+    criteria <- sapply(res, function(x){x$mdl})
     objlist[[nbreaks]] <- list(nbreaks=nbreaks,pop=pop,criteria=criteria)
 
     # updating
     min.ind <- which.min(criteria)
     min.criteria <- min(criteria)
+    best.criteria.seq <- c(best.criteria.seq, min.criteria)
     if (min.criteria < old.min.criteria){
       #best.obj <- objlist[[nbreaks]]
       cbreaks <- c(cbreaks,breaks.ind[min.ind])
@@ -90,14 +139,20 @@ spec.tbreak <- function(x1, x2, y, A, delta.t, delta.w,
     } else {
       cont <- F
     }
+
+    # construct chrom0stat for next iteration
+    if (cont){
+      chrom0 <- (rep(F,n1-1)); chrom0[cbreaks] <- TRUE
+      chrom0stat <- list(chrom=chrom0, loglike=res[[min.ind]]$loglike, mdl.pen=res[[min.ind]]$mdl.pen)
+    }
   }
   if (cpus>1) parallel::stopCluster(cl)
 
   # final fit
   minx1 <- min(x1)-1
   maxx1 <- max(x1)+1
-  best.chrom <- (rep(F,n1-1))
-  best.chrom[cbreaks] <- T
+  best.chrom <- (rep(FALSE,n1-1))
+  best.chrom[cbreaks] <- TRUE
   B <- sum(best.chrom)+1
   x1.index <- cut(x1,breaks=c(minx1,x1[c(FALSE,best.chrom)],maxx1),labels=(1:B),right=FALSE)
   x1.len <- tapply(x1,x1.index,length)
@@ -106,6 +161,30 @@ spec.tbreak <- function(x1, x2, y, A, delta.t, delta.w,
     tempy <- apply(y[,x1.index==k],1,sum)
     best.fit[[k]] <- spec(x=x2, y=tempy, A=A, delta.t=delta.t, delta.w=delta.w,reps=x1.len[k],display=F,opt=opt,assign.emiss=assign.emiss,emdl=emdl,v=v1,simple=simple)
   }
-  return(list(B=B,best.fit=best.fit,breaks=c(1,cbreaks+1),content=objlist, v1=v1, v2=v2))
+  return(list(B=B,best.fit=best.fit,breaks=c(1,cbreaks+1),content=objlist,
+              best.criteria.seq=best.criteria.seq, opt=opt,
+              assign.emiss=assign.emiss, emdl=emdl, v1=v1, v2=v2, max.B=max.B,
+              min.span=min.span))
 }
 
+
+########################
+#### heatmap matrix ####
+########################
+# fit corresponds to out$best.fit
+# breaks corresponds to out$breaks
+
+create.specheatmap.matrix <- function(x1, x2, breaks, fit, log=FALSE){
+  B <- length(breaks)
+  temp <- matrix(nr=length(x2), nc=length(x1))
+  tbreaks <- sort(c(breaks,length(x1)+1))
+  for (j in (1:B)){
+    temp[,tbreaks[j]:(tbreaks[j+1]-1)] <- (fit[[j]]$fitted.logbright)
+  }
+  dimnames(temp) <- list(x2, x1)
+  if (log){
+    return(t(temp))
+  } else {
+    return(t(exp(temp)))
+  }
+}
--- R/spectrum.R
+++ R/spectrum.R
@@ -22,6 +22,14 @@ get.opt.spec <- function(p=3, K=30, alpha=1, nlambda=100, bs="rb", lam.ratio.lim
 spec <- function(x, y, A, delta.t, delta.w, reps=1, display=TRUE,
                  opt=get.opt.spec(), assign.emiss=NA, emdl=TRUE, simple=TRUE, v=1){
 
+  # for assign.emiss=NULL, nlambda should be greatly increased
+  # it will be automatically set to 10000 if it is set smaller
+  # if this change is not desired, one can set lam.ratio.len=-1
+  # note: lam.ratio.len is useless in the setting of assign.emiss=NULL
+  if ((is.null(assign.emiss))&&(opt$lam.ratio.len!=-1)){
+    if (opt$nlambda<10000) opt$nlambda <- 10000
+  }
+
   # assign options
   p<-opt$p
   K<-opt$K
@@ -37,8 +45,10 @@ spec <- function(x, y, A, delta.t, delta.w, reps=1, display=TRUE,
   # main
   n <- length(y)
   off.const <- log(delta.t)+log(delta.w)+log(A)+log(reps)
-  if (is.na(assign.emiss)) assign.emiss <- 1:n
-  assign.emiss <- sort(assign.emiss)
+  if (!is.null(assign.emiss)){
+    if (is.na(assign.emiss)) assign.emiss <- 1:n
+    assign.emiss <- sort(assign.emiss)
+  }
 
 
   # select splines
@@ -54,20 +64,28 @@ spec <- function(x, y, A, delta.t, delta.w, reps=1, display=TRUE,
   }
 
   # create data matrix for atoms
-  B.atoms <- as.matrix(diag(rep(1,n))[,assign.emiss])
   n1 <- ncol(B.splines)
-  n2 <- ncol(B.atoms)
-  X <- cbind(B.splines,B.atoms)
+  if (is.null(assign.emiss)){
+    n2 <- 0
+    X <- B.splines
+    lam.ratios <- 1
+    lam.ratio.len <- 1
+  } else {
+    B.atoms <- as.matrix(diag(rep(1,n))[,assign.emiss])
+    n2 <- ncol(B.atoms)
+    X <- cbind(B.splines,B.atoms)
+    lam.ratios <- seq(lam.ratio.lims[1],lam.ratio.lims[2],len=lam.ratio.len)
+  }
 
   # for ratios
   #lam.ratios <- exp(seq(log(lam.ratio.lims[1]),log(lam.ratio.lims[2]),len=lam.ratio.len))
-  lam.ratios <- seq(lam.ratio.lims[1],lam.ratio.lims[2],len=lam.ratio.len)
   mdl <- matrix(nrow=lam.ratio.len,ncol=nlambda)
   mdl.pen <- matrix(nrow=lam.ratio.len,ncol=nlambda)
   beta.cube <- array(dim=c(lam.ratio.len,nlambda,n1+n2+1))
   lams <- matrix(nrow=lam.ratio.len,ncol=nlambda)
 
   # for display
+  if (lam.ratio.len < 50) display <- FALSE
   if(display){
     cat(paste(c("|",rep(".",50),"|\n","|"),collapse=""))
     display.count <- 1
@@ -85,16 +103,26 @@ spec <- function(x, y, A, delta.t, delta.w, reps=1, display=TRUE,
     } else if (bs=="bs"){
       fit <- glmnet::glmnet(x=X,y=y,family="poisson",offset=off.const,alpha=alpha,nlambda=nlambda,penalty.factor=c(rep(lam1,n1),rep(lam2,n2)),lambda.min.ratio=glmnet.lam.min)
     }
-    # computing mdl
-    nonzeros <- apply(coef(fit),2,function(x,n1,n2){c(sum(x[1:(n1+1)]!=0),sum(x[(n1+2):(n1+n2+1)]!=0))},n1=n1,n2=n2)
-    dev <- (1-fit$dev.ratio)*fit$nulldev
-    #mdl.pen[i,] <- log(n*reps)*(fit$df+1)+2*bgamma*lchoose(n2,nonzeros[2,])
-    #mdl[i,] <- dev + mdl.pen[i,]
-    n3 <- length(fit$lambda)
-    lams[i,1:n3] <- fit$lambda
-    mdl.pen[i,1:n3] <- log(n*reps)*(fit$df+1)/2+bgamma*lchoose(n2,nonzeros[2,])*emdl
-    mdl[i,1:n3] <- dev/2 + mdl.pen[i,1:n3]*v
-    beta.cube[i,1:n3,] <- t(as.matrix(coef(fit)))
+
+    temp <- try(coef(fit), silent=TRUE)
+
+    if (class(temp)=="try-error"){
+      mdl[i,] <- Inf
+      mdl.pen[i,] <- Inf
+    } else {
+      # computing mdl
+      nonzeros <- apply(temp,2, function(x,n1,n2){c(sum(x[1:(n1+1)]!=0),sum(x[(n1+2):(n1+n2+1)]!=0))} ,n1=n1,n2=n2)
+      if (is.null(assign.emiss)) nonzeros[2,] <- 0
+      dev <- (1-fit$dev.ratio)*fit$nulldev
+      #mdl.pen[i,] <- log(n*reps)*(fit$df+1)+2*bgamma*lchoose(n2,nonzeros[2,])
+      #mdl[i,] <- dev + mdl.pen[i,]
+      n3 <- length(fit$lambda)
+      lams[i,1:n3] <- fit$lambda
+      mdl.pen[i,1:n3] <- log(n*reps)*(fit$df+1)/2+bgamma*lchoose(n2,nonzeros[2,])*emdl
+      # since nonzeros[2,] is assigned to 0 if assign.emiss=NULL, the last term will be automatically zero
+      mdl[i,1:n3] <- dev/2 + mdl.pen[i,1:n3]*v
+      beta.cube[i,1:n3,] <- t(as.matrix(coef(fit)))
+    }
     # for display
     if (display){
       if ((i/lam.ratio.len*50)>=display.count) {
@@ -109,18 +137,25 @@ spec <- function(x, y, A, delta.t, delta.w, reps=1, display=TRUE,
 
   # output fit
   mdl.ind <- arrayInd(which.min(mdl),dim(mdl))
-  if ((min(mdl.ind)==1)||(mdl.ind[1]==lam.ratio.len)||(mdl.ind[2]==nlambda)){
-    warnings("mdl: require a larger range\n")
+  if (is.null(assign.emiss)){
+    if (mdl.ind[2]==nlambda){
+      warnings("mdl: require a larger range\n")
+    }
+  } else {
+    if ((min(mdl.ind)==1)||(mdl.ind[1]==lam.ratio.len)||(mdl.ind[2]==nlambda)){
+      warnings("mdl: require a larger range\n")
+    }
   }
   best.beta <- as.vector(beta.cube[mdl.ind[1],mdl.ind[2],])
-  fitted.bright <- as.vector(best.beta[1] + X%*%best.beta[-1])
-  fitted.bright.ne <- as.vector(best.beta[1] + B.splines%*%best.beta[2:(n1+1)])
+  fitted.logbright <- as.vector(best.beta[1] + X%*%best.beta[-1])
+  fitted.logbright.ne <- as.vector(best.beta[1] + B.splines%*%best.beta[2:(n1+1)])
   df <- c(sum(best.beta[2:(n1+1)]!=0),sum(best.beta[(n1+2):(n1+n2+1)]!=0))
+  if (is.null(assign.emiss)) df[2] <- 0
   out.ind <- assign.emiss[which(best.beta[(n1+2):(n1+n2+1)]!=0)]
   if (simple){
     beta.cube <- NULL
   }
-  return(list(best.beta=best.beta,fitted.bright=fitted.bright,fitted.bright.ne=fitted.bright.ne,df=df,out.ind=out.ind,mdl.ind=mdl.ind,mdl=mdl,mdl.pen=mdl.pen,beta.cube=beta.cube,reps=reps,off.const=off.const))
+  return(list(best.beta=best.beta,fitted.logbright=fitted.logbright,fitted.logbright.ne=fitted.logbright.ne,df=df,out.ind=out.ind,mdl.ind=mdl.ind,mdl=mdl,mdl.pen=mdl.pen,beta.cube=beta.cube,reps=reps,off.const=off.const))
 }
 
 
@@ -129,11 +164,19 @@ spec <- function(x, y, A, delta.t, delta.w, reps=1, display=TRUE,
 ###########################
 # if there are more utility funciton, we may want to write a Class
 
-plotspec <- function(x, fit, np=TRUE, ...){
+plotspec <- function(x, fit, np=TRUE, transform=NULL, ...){
   n <- length(x)
   delta.w <- (x[2]-x[1])
-  bright <- exp(fit$fitted.bright)
-  bright.ne <- exp(fit$fitted.bright.ne)
+  if (is.null(transform)){
+    bright <- exp(fit$fitted.logbright)
+    bright.ne <- exp(fit$fitted.logbright.ne)
+  } else if (transform=="log"){
+    bright <- fit$fitted.logbright
+    bright.ne <- fit$fitted.logbright.ne
+  } else if (transform=="log10"){
+    bright <- fit$fitted.logbright / log(10)
+    bright.ne <- fit$fitted.logbright.ne / log(10)
+  }
   if (np){
     plot(x,bright,type="n",xlim=c(x[1]-delta.w/2,x[n]+delta.w/2),...)
   }
